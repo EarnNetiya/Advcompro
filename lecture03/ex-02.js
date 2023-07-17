a31c0fade3172eaee42af618d69e5452c190aeff
EarnNetiya <s6506022630015@email.kmutnb.ac.th>
2023-07-17 20:14:25 +0700

    gggggg

---
+++ lecture03/ex-02.js
@@ -0,0 +1,9 @@
+let furniture = ['Table', 'Chairs', 'Couch'];
+
+for (let i = 0; i < furniture.length; i++) {
+  let item = furniture[i]
+  for (let x = 0; x < item.length; x++){
+    console.log(item[x])
+  }
+
+}

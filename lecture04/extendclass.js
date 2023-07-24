8a11b72bf4b9fdc8a923c48bbf0dc288c93d6cac
EarnNetiya <s6506022630015@email.kmutnb.ac.th>
2023-07-24 19:03:57 +0700

    new

--- lecture04/extendclass.js
+++ lecture04/extendclass.js
@@ -1,5 +1,19 @@
 class Media {
     constructor(into) {
         this.publicDate = into.publicDate
+        this.name = info.name;
     }
 }
+
+class Song extends Media {
+    constructor(songData) {
+        super(songData);
+        this.artist = songData.artist;
+    }
+}
+
+const mySong = new Song({
+    artist: "Queen",
+    name: "Bohemain Rhapsody",
+    publicDate: 1975
+})
